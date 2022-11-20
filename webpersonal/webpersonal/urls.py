58e07ef09f8385b27f59dc2dae84887ad10e4229
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-11-20 14:35:57 +0100

    cambios

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -19,5 +19,7 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
